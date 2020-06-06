02c5ac983cacb3090850a80c5e4bd459e8c02770
Paula Boyano <paula.boyano@est.fib.upc.edu>
2020-06-06 10:01:54 +0200

    gitignore added

--- bot.py
+++ bot.py
@@ -3,6 +3,10 @@ from telegram.ext import Updater, CommandHandler
 
 from telegram import ParseMode
 
+from skyline import Skyline
+
+import pickle
+
 # hace las inicializaciones pertinentes y da la bienvenida
 def start(update, context):
     botname = context.bot.username
@@ -30,6 +34,44 @@ def author(update, context):
         chat_id=update.effective_chat.id, 
         text=mssg)
 
+def createSkyline(update, context):
+    print("entro en la func createSkyline")
+    id = str(context.args[0])
+    print("pillo el primer arg")
+    x = int(context.args[1])
+    print("pillo el sec arg")
+    y = int(context.args[2])
+    print("pillo el terc arg")
+    x2 = int(context.args[3])
+    print("pillo el quart arg")
+
+    s = Skyline(id, x, y, x2)
+    print("creo skyline")
+
+    s.plotProcessing()
+    print("hago el plotProcessing")
+
+    id2 = id + '.png'
+    print(id2)
+    context.bot.send_photo(
+        chat_id=update.effective_chat.id,
+        photo=open(id2,'rb'))
+    print("supuestamente despues de enviar el mensaje")
+
+def save(update, context):
+    id = str(context.args[0])
+    s = Skyline(id, x, y, x2)
+    filename = id +'.sky'
+    outfile = open(filename, 'wb')
+    pickle.dump(s,outfile)
+    outfile.close()
+
+def load(update, context):
+    id = str(context.args[0])
+    filename = id + '.sky'
+    infile = open(filename, 'rb')
+    s = pickle.load(infile)
+    infile.close()
 
 # declara una constant amb el access token que llegeix de token.txt
 TOKEN = open('token.txt').read().strip()
@@ -42,6 +84,9 @@ dispatcher = updater.dispatcher
 dispatcher.add_handler(CommandHandler('start', start))
 dispatcher.add_handler(CommandHandler('help', help))
 dispatcher.add_handler(CommandHandler('author', author))
+
+dispatcher.add_handler(CommandHandler('createSkyline', createSkyline))
+
 #dispatcher.add_handler(CommandHandler('lst', lst))
 #dispatcher.add_handler(CommandHandler('clean', clean))
 #dispatcher.add_handler(CommandHandler('save', save))
--- skyline.py
+++ skyline.py
@@ -1,15 +1,39 @@
+import matplotlib.pyplot as plt
+
 class Skyline:
+
     def __init__(self, id, xmin, height, xmax):
         b1 = (xmin, height, xmax)
-        bl = [ed1]
+        bl = [b1]
         self.id = id
         self.bl = bl  # bl = buildings list
 
+    # ##########GETTERS########### #
+
+    # returns the skyline's buildings list
     def getBuildingsList(self):
         return bl
 
+    # returns the skyline's id
     def getID(self):
         return id
+
+    # retunrs the slyline's area
+    def getArea(self):
+        sum = 0
+        for k in self.bl:
+            sum += buildingArea(k)
+        return sum
+
+    # returns the skyline's height
+    def getHeight(self):
+        max = 0
+        for k in self.bl:
+            if k[1] > max:
+                max = k[1]
+        return max
+
+    # ########OPERATIONS########## #
 
     # add a building to the current skyline
     def addBuilding(self, xmin, height, xmax):
@@ -23,6 +47,10 @@ class Skyline:
     def intersecSkyline(self, skyobj):
         bl2 = skyobj.getBuildingsList()
         self.bl = listIntersection(self.bl, bl2)
+
+    # do the refelction of the current skyline
+    def mirrorSkyline(self):
+        self.bl = self.bl.reverse()
 
     # replicate the skyline n times
     def replicateSkyline(self, n):
@@ -39,10 +67,34 @@ class Skyline:
         for k in self.bl:
             k[0] -= n
 
-    # UTILS #
+    # ########PROCESSING########## #
+
+    def plotProcessing(self):
+        xl = []
+        widthl = []
+        heightl = []
+        for k in self.bl:
+            w = k[2] - k[0]
+            xl += [k[0]]
+            widthl += [w]
+            heightl += [k[1]]
+        plt.bar(xl, heightl, widthl, 0, align='edge')
+        plt.savefig(str(self.id))
+
+
+
+    # ###########UTILS############ #
+
+    # returns the intersection of lists a and b
     def listIntersection(a, b):
         result = []
-        for i in a:
-            if (i not in result) and (i in b):
-                result.append(i)
+        for k in a:
+            if (k not in result) and (k in b):
+                result.append(k)
         return result
+    
+    def buildingArea(b):
+        y = b[1]
+        z = b[2]
+        x = b[0]
+        return y*(z-x)
